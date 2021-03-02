071fc2a8ce8a917ceb20e053072fbbb7470e087c
Ethan Zitting <ethanzitting@gmail.com>
2021-03-02 08:40:18 -0700

    Changed fonts away from Google Fonts for load times.

--- src/index.js
+++ src/index.js
@@ -14,18 +14,3 @@ persBtn.addEventListener('click', () => {
   loadPers();
 })
 
-/*
- -Redo resume to match Kenton's format
- -Drop "I have a ... mind and ..."
- -"I'm a skilled project manager, business starter, and tech whisperer
- -"I'm a very driven individual".
- -"Strong, team-oriented"
- -Amazon: "Received accolates for time management on my routes".
- -Capstone: "Initial Clientel Acquisition".
- -Zitting Steel: "Researched, Strategizeds, and Implemented Production Process Upgrades"
- --: Less masterbation in description.
- -Slipwell: Reword First One
- -Move Epsilon over to Work Experience
- -'Ops Management in Education
- -Recent Professional Achievements
-*/
