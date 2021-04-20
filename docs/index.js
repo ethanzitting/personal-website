12a0e66db118e43d2975826a01a6e2d5349489af
Ethan Zitting <ethanzitting@gmail.com>
2021-04-20 10:32:50 -0500

    website loads websites previews that match your device.

--- docs/index.js
+++ docs/index.js
@@ -86,18 +86,19 @@ window.mobileCheck = function() {
 };
 
 // Generate the HTML for each element accordingly
-let mobileCheck = window.mobileCheck();
+let userOnMobile = window.mobileCheck();
 
 for (let i = 0; i < portfolio.length; i++) {
   portfolioDiv.innerHTML += `
-  <a href="${portfolio[i].link}" target='_blank' rel='noreferrer'>
-    <div class="card">
-    <img class="card-img-top" src="${portfolio[i].desktopSrc}" alt="${portfolio[i].alt}">
-      <div class="card-body">
-        <h7 class="card-title">${portfolio[i].title}</h7>
+    <a href="${portfolio[i].link}" target='_blank' rel='noreferrer'>
+      <div class="card">
+      <img class="card-img-top" src="${userOnMobile ? portfolio[i].mobileSrc : portfolio[i].desktopSrc}" alt="${portfolio[i].alt}">
+        <div class="card-body">
+          <h7 class="card-title">${portfolio[i].title}</h7>
+        </div>
       </div>
-    </div>
-  </a>`
+    </a>
+  `
 }
 
 /* Fixes broken mobile navbar hamburger button */
