5e487b3b7704bf446ffcca3208787a1895b25104
ethanzitting@gmail.com <ethanzitting@gmail.com>
2021-08-01 09:41:03 -0500

    removed npm, set background pattern, set base body and main behav., started main content, layout somewhat responsive

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let portfolio = [
   {
     title: 'Client: Silver Ridge',
     link: 'https://silverridgecontracting.com',
-    alt: "preview of Ethan Zitting's work on silverridgeconsulting.com",
+    alt: "preview of Ethan Zitting's work on this website",
     mobileSrc: './images/websites/silver-mb.png',
     desktopSrc: './images/websites/silver-dt.png'
   },
   {
     title: 'Client: Epsilon',
     link: 'https://epsilonintegratedsolutions.com',
-    alt: "preview of Ethan Zitting's work on epsilonintegratedsolutions.com",
+    alt: "preview of Ethan Zitting's work on this website",
     mobileSrc: './images/websites/epsilon-mb.png',
     desktopSrc: './images/websites/epsilon-dt.png'
   },
@@ -36,41 +36,6 @@ let portfolio = [
   },
 ];
 
-let workHistory = [
-  {
-    title: "Delivery Driver",
-    company: "Celestine Logistics",
-    focus: "Standard Package Delivery",
-    description: `I'm using this position to pay my living expenses while studying programming.
-    Here I've gained a deep understanding of the extremely high efficiency of modern parcel delivery techniques.
-    I've been able to practice steady, careful attention to detail for long durations.
-    I've honestly had a lot of fun exploring basically every part of Colorado Springs and the surrounding towns.
-    And, I've been powering through my to-do list of audiobooks.`,
-  },
-  {
-    title: "Project Manager",
-    company: "Zitting Steel",
-    focus: "Project Management, Construction R&D, Conflict Resolution",
-    description: `I was brought on to research, plan, and carry out the upgrade of our fabrication facilitie's quality control processes to meet AISC compliance.
-    After doing this and demonstrating with it how quickly I can take on new projects and learn new skills, I was put in charge of managing the installation of our steel on a large residential construction site.
-    Our work on this jobsite had been gridlocked for 4+ months, and there was a great deal of ill-will as a result.
-    I quickly got production moving again.
-    I learned BluBeam bidding software and completed several of our bids.
-    Errors and conflict were greatly reduced, and work has been completed.`,
-  },
-  {
-    title: "C.O.O.",
-    company: "Slipwell Industries",
-    focus: "Business Development, Construction R&D, Project Management",
-    description: `Here I lead the R&D of a proprietary new concrete tower production machine.
-    I learned Onshape 3D Design software and lead the design and testing of our machine.
-    I worked with an outside structral engineer to update and refine the designs.
-    I establish the legal entity, company structure, and business habits.
-    I kept the business books and filed the taxes.
-    I oversaw the production of marketing material.
-    I managed the social media and online presence of the company.`,
-  }
-];
 
 const $ = (element) => document.querySelector(element);
 
@@ -101,14 +66,5 @@ for (let i = 0; i < portfolio.length; i++) {
     </a>
   `
 }
-
-/* Fixes broken mobile navbar hamburger button */
-
-const navbarToggler = $('.navbar-toggler');
-const collapsingMenu = $('.navbar-collapse');
-
-navbarToggler.addEventListener('click', () => {
-  collapsingMenu.classList.toggle('show');
-})
 
 
