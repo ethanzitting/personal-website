517daedca0c12e32746a2b34637c11d597c9cbab
Ethan Zitting <ethan@ethanzitting.com>
2021-09-21 08:52:28 -0500

    improved behavior of slider images, size, and slider arrows

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,6 @@
 import $ from 'jquery';
 import 'slick-slider';
-// import '../scss/index.scss';
-import '../scss/styles.scss';
-import '../scss/reset.scss';
-
-import '../../node_modules/slick-slider/slick/slick.scss';
-import '../../node_modules/slick-slider/slick/slick-theme.scss';
-
+import '../scss/index.scss';
 
 let portfolio = [
     {
