f34400408dd6e51ec6c6c403b343eb3aab5ef857
Ethan Zitting <ethan@ethanzitting.com>
2021-09-21 08:15:09 -0500

    browser-sync first functionality established

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 const config = {
     mode: 'development',
@@ -13,6 +14,24 @@ const config = {
         new MiniCssExtractPlugin({
             linkType: "text/css",
         }),
+        new BrowserSyncPlugin(
+            // BrowserSync options
+            {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:8000/)
+                // through BrowserSync
+                proxy: 'http://localhost:8000/'
+            },
+            // plugin options
+            {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+            }
+        ),
     ],
     module: {
         rules: [
